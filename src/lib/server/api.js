804f4c2f2e91d8d4fe53a83575226c431cbe972a
Johan Haugland <johanhaugl@gmail.com>
2022-09-15 10:44:54 +0200

    Refactor: rewrite to variables

--- src/lib/server/api.js
+++ src/lib/server/api.js
@@ -1,11 +1,16 @@
+const dbID = 'fd610b0c-2480-4c5a-81c2-494879cdf141';
+const region = 'europe-west1';
+const ns = 'feedback_app_store';
+const token =
+	'AstraCS:jiHYMZCMfIbABDucnShDknAo:1830439e0388064c75b989110194f007b9c96fc721032d7d54a45227810b6c8f';
+const midURL = 'apps.astra.datastax.com/api/rest/v2';
+const collection = 'feedback_collection';
+
 const headers = {
 	'Content-Type': 'application/json',
-	'X-Cassandra-Token':
-		'AstraCS:jiHYMZCMfIbABDucnShDknAo:1830439e0388064c75b989110194f007b9c96fc721032d7d54a45227810b6c8f'
+	'X-Cassandra-Token': token
 };
-const url =
-	'https://fd610b0c-2480-4c5a-81c2-494879cdf141-europe-west1.apps.astra.datastax.com/api/rest/v2/namespaces/feedback_app_store/collections/test_collection';
-
+const url = `https://${dbID}-${region}.${midURL}/namespaces/${ns}/collections/${collection}`;
 export const testApi = () => {
 	const response = {
 		a: '1',
@@ -16,9 +21,8 @@ export const testApi = () => {
 
 export const getCatFact = fetch('https://catfact.ninja/fact').then((response) => response.json());
 
-export const getFeedback = fetch(url, { headers })
+export const getFeedback = fetch(url, { method: 'GET', headers })
 	.then(function (response) {
-		console.log(response);
 		if (!response.ok) {
 			throw Error(`${response.status}, ${response.statusText}`);
 		}
